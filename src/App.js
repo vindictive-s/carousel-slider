75bb8914b427200effcd167d83435659b52b579e
vindictive-s <se7bastian.bosoi+github@gmail.com>
2023-02-02 08:41:50 +0000

    default config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React from "react";
+import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
 
 function App() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 0,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1050,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 0,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="carousel">
       <h2>Carousel Sample</h2>
-      <div className="box">
-        <h3>1</h3>
-      </div>
-      <div className="box">
-        <h3>2</h3>
-      </div>
-      <div className="box">
-        <h3>3</h3>
-      </div>
-      <div className="box">
-        <h3>4</h3>
-      </div>
-      <div className="box">
-        <h3>5</h3>
-      </div>
+      <Slider {...settings}>
+        <div className="box">
+          <h3>1</h3>
+        </div>
+        <div className="box">
+          <h3>2</h3>
+        </div>
+        <div className="box">
+          <h3>3</h3>
+        </div>
+        <div className="box">
+          <h3>4</h3>
+        </div>
+        <div className="box">
+          <h3>5</h3>
+        </div>
+      </Slider>
     </div>
   );
 }
