0e7744f034060697ba4f2346ed87b568fbdd8b89
vindictive-s <se7bastian.bosoi+github@gmail.com>
2023-02-02 08:06:23 +0000

    carousel slider sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="carousel">
+      <h2>Carousel Sample</h2>
+      <div className="box">
+        <h3>1</h3>
+      </div>
+      <div className="box">
+        <h3>2</h3>
+      </div>
+      <div className="box">
+        <h3>3</h3>
+      </div>
+      <div className="box">
+        <h3>4</h3>
+      </div>
+      <div className="box">
+        <h3>5</h3>
+      </div>
     </div>
   );
 }
